105b296c59b9c40e7a861bdb6b90ce8bc0f62e2d
ricker <105864095+ricardoluzfr@users.noreply.github.com>
2022-05-30 15:38:10 -0300

    Delete empiricus directory

--- empiricus/js/year.js
+++
@@ -1,6 +0,0 @@
-/* JS bem simples que mantem o ano atualizado nas páginas */
-var date = new Date();
-var year = date.getFullYear();
-console.log(year);
-
-document.getElementById('year').innerHTML = year;
